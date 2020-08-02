6cc8229f4a31c17c7c7820bee5ee0b93961d01f4
Francis Albuna <falbuna1@gmail.com>
2020-08-02 08:30:55 -0700

    Got moment.js to display the date. Added placeholder containers for the time blocks.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const nowDay = moment().format('dddd, MMMM Do');
+
+const displayday = document.getElementById('currentDay');
+displayday.innerHTML = nowDay;
+
 const hours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
 
 const container = $('#main-planner');
@@ -10,11 +15,20 @@ hours.forEach(function (hour){
 
     const hourDiv = $('<div>');
     hourDiv.attr('id');
-
     hourDiv.text(hour);
     hourDiv.addClass('col-md-3 hour')
 
     rowDiv.append(hourDiv);
+
+    const inputDiv = $('<form>');
+    inputDiv.addClass('col-md-8 past')
+
+    rowDiv.append(inputDiv)
+
+    const saveBtnDiv = $('<button>');
+    saveBtnDiv.addClass('col-md-1 saveBtn');
+
+    rowDiv.append(saveBtnDiv);
  
     container.append(rowDiv);
 })
