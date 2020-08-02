b4c2fc2ca57a1d106c9872880958624805f7b5f9
Francis Albuna <falbuna1@gmail.com>
2020-08-02 11:08:35 -0700

    Got the forEach loop to work to color code the time of day.

--- script.js
+++ script.js
@@ -3,7 +3,9 @@ const nowDay = moment().format('dddd, MMMM Do');
 const displayday = document.getElementById('currentDay');
 displayday.innerHTML = nowDay;
 
-const hours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
+const hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+
+const nowTime = moment().format('h')
 
 const container = $('#main-planner');
 
@@ -14,15 +16,25 @@ hours.forEach(function (hour){
     rowDiv.addClass('row time-block');
 
     const hourDiv = $('<div>');
-    hourDiv.attr('id');
-    hourDiv.text(hour);
-    hourDiv.addClass('col-md-3 hour')
+    hourDiv.attr('id', 'hour-' + hour);
+
+    const currentHour = hour === 12 ? 12 : hour % 12;
+    const amOrPm = (hour > 11) ? 'PM' : 'AM';
+
+    hourDiv.text(currentHour + '' + amOrPm);
+    hourDiv.addClass('col-md-3 hour');
 
     rowDiv.append(hourDiv);
 
-    const inputDiv = $('<form>');
-    inputDiv.addClass('col-md-8 past')
-
+    const inputDiv = $('<textarea>');
+    if ( hour < nowTime){
+        inputDiv.addClass('col-md-8 past');}
+    else if ( hour > nowTime){
+        inputDiv.addClass('col-md-8 future');
+    }
+    else {
+        inputDiv.addClass('col-md-8 present');}
+    
     rowDiv.append(inputDiv)
 
     const saveBtnDiv = $('<button>');
