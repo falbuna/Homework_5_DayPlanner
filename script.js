e9f866550ec8e12ff4f94a49fabcda2be3f63309
Francis Albuna <falbuna1@gmail.com>
2020-08-02 18:35:07 -0700

    Added event listener for clicking to save button. Attempted to save to local storage but needs work.

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-const nowDay = moment().format('dddd, MMMM Do');
+// This will get the current date from moment.js in Day, Month, Day of the Month format.
+const DayToday = moment().format('dddd, MMMM Do');
 
+// This will display the current day on the main page.
 const displayday = document.getElementById('currentDay');
-displayday.innerHTML = nowDay;
+displayday.innerHTML = DayToday;
 
+// This array holds all of the hours that will display.
 const hours = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-const nowTime = moment().format('h')
+// This will get the current hour from moment.js.
+const nowTime = moment().format('HH')
 
+// Main Container for the planner.
 const container = $('#main-planner');
 
+// This forEach loop will create a timeblock for all the hours.
 hours.forEach(function (hour){
-
-    const rowDiv = $('<div>');
+    // This will create the row for the hour blocks.
+    const rowDiv = $('<form>');
 
     rowDiv.addClass('row time-block');
 
+    // This will create the hour container
     const hourDiv = $('<div>');
     hourDiv.attr('id', 'hour-' + hour);
 
@@ -22,27 +29,50 @@ hours.forEach(function (hour){
     const amOrPm = (hour > 11) ? 'PM' : 'AM';
 
     hourDiv.text(currentHour + '' + amOrPm);
-    hourDiv.addClass('col-md-3 hour');
+    hourDiv.addClass('col-3 hour');
 
     rowDiv.append(hourDiv);
-
+    // This will create the input container that will hold the text area.
     const inputDiv = $('<textarea>');
+    inputDiv.attr('class', 'plannerText')
     if ( hour < nowTime){
-        inputDiv.addClass('col-md-8 description past');}
+        inputDiv.addClass('col-8 description past');}
     else if ( hour > nowTime){
-        inputDiv.addClass('col-md-8 description future');
+        inputDiv.addClass('col-8 description future');
     }
     else {
-        inputDiv.addClass('col-md-8 description present');}
+        inputDiv.addClass('col-8 description present');}
     
     rowDiv.append(inputDiv)
-
-    const saveBtnDiv = $('<button>');
+    // This will create the save button for each of the hour blocks.
+    var saveBtnDiv = $('<button>');
     saveBtnDiv.attr('class', 'fa fa-save');
-    saveBtnDiv.addClass('col-md-1 saveBtn');
+    saveBtnDiv.addClass('col-1 saveBtn');
 
     rowDiv.append(saveBtnDiv);
  
     container.append(rowDiv);
+
+
+    $('.saveBtn').on('click', function(event){
+        event.preventDefault();
+        console.log("I've been clicked!");
+    
+        var Stuff = $('.plannerText').val().trim();
+    
+        localStorage.setItem('plannerStorage', JSON.stringify(Stuff));
+        console.log(localStorage)
+    });
 })
 
+
+    // $('.saveBtn').on('click', function(){
+    //     event.preventDefault();
+    //     console.log("I've been clicked!");
+
+    //     var Stuff = document.querySelector('#plannerText').value;
+    
+    //     localStorage.setItem('plannerStuff', Stuff);
+    //     console.log(localStorage)
+
+    // })
